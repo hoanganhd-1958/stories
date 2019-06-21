ead999de42a3c1d4df5512b0454b1d29a3905a6d
hoanganhhd-1958 <doan.hoang.anh@sun-asterisk.com>
2019-06-21 09:57:11 +0700

    13909_setup_migration

--- database/migrations/2019_06_21_034914_create_stories_table.php
+++ database/migrations/2019_06_21_034914_create_stories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateStoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('stories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('summary');
+            $table->text('cover_image');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
@@ -33,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('stories');
     }
 }
