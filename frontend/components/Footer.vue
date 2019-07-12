798c601c2e3c913a98f731798ac27cc276104903
hoanganhd-1958 <doan.hoang.anh@sun-asterisk.com>
2019-07-12 14:43:06 +0700

    14874_home_page

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -3,7 +3,7 @@
         <p>
             <b>TruyenAZ</b> - Đọc truyện online, đọc truyện chữ, truyện hay, nhiều hơn thế. Website luôn cố gắng cập nhật
             những bộ truyện mới gồm nhiều thể loại đặc sắc như truyện tiên hiệp, truyện kiếm hiệp, truyện ma, ngôn tình
-            hay truyện teen nhanh nhất. Hỗ trợ đọc trên mọi thiết bị như máy tình để bàn, điện thoại, máy tính bảng.
+            hay truyện teen nhanh nhất. Hỗ trợ đọc trên mọi thiết bị như máy tình để bàn, điện thoại, máy tính bảng
         </p>
         <p>
             Hiện tại hệ thống của chúng tôi vẫn đang trong quá trình chạy thử nghiệm nhằm thu thập phản hồi,
